d7e8735c68663274e361ea9f43a7ee04b3b91f1a
Jack Ren <bjrjk@qq.com>
2020-08-28 13:18:53 +0800

    Initial Commit

---
+++ nCov-Patient-Tracer/Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nCov_Patient_Tracer
+{
+    class Global
+    {
+        public const string WebURL = "https://ncov-patient-tracer.renjikai.com/";
+    }
+}
---
+++ nCov-Patient-Tracer/frmMain.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nCov_Patient_Tracer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("作者：18041403 任骥恺", "关于");
+        }
+
+        private void frmMain_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            web.Height = grdContainer.ActualHeight - 25;
+            web.Address = Global.WebURL;
+        }
+    }
+}
