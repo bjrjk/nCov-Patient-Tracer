98206556e6c09c18fef5ca32df7796d03498a859
Jack Ren <bjrjk@qq.com>
2020-09-04 10:02:23 +0800

    20200904 1002

--- nCov-Patient-Tracer/DSA/IntervalTree.cs
+++ nCov-Patient-Tracer/DSA/IntervalTree.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace nCov_Patient_Tracer.DSA
 {
-    class IntervalTree
+    class IntervalTree<T>where T:ISegment
     {
+        struct Point
+        {
+            int pos;
+
+            T obj;
+        }
+    }
+    interface ISegment
+    {
+        int getStartPoint();
+        int getEndPoint();
+        bool InterSection(ISegment seg);
+        bool InterSection(int point);
     }
 }
