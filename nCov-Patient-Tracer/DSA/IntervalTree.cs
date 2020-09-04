110f55fbf2777d3f59ede9d9547458b088a55235
Jack Ren <bjrjk@qq.com>
2020-09-04 14:54:19 +0800

    20200904 1454

--- nCov-Patient-Tracer/DSA/IntervalTree.cs
+++ nCov-Patient-Tracer/DSA/IntervalTree.cs
@@ -6,13 +6,149 @@ using System.Threading.Tasks;
 
 namespace nCov_Patient_Tracer.DSA
 {
-    class IntervalTree<T>where T:ISegment
+    class IntervalTree<T> where T : ISegment
     {
-        struct Point
+        private class Point : IComparable<Point>
         {
-            int pos;
-
-            T obj;
+            public int pos;
+            public bool isStart;
+            public T obj;
+            public Point(int pos, bool isStart, T obj)
+            {
+                this.pos = pos;
+                this.isStart = isStart;
+                this.obj = obj;
+            }
+            public int CompareTo(Point other)
+            {
+                return pos.CompareTo(other.pos);
+            }
+        }
+        private class PointComparer : IComparer<Point>
+        {
+            public int Compare(Point x, Point y)
+            {
+                return x.pos.CompareTo(y.pos);
+            }
+        }
+        private class Node
+        {
+            public Node lc, rc;
+            public int Xmid;
+            public Vector<Point> leftList, rightList;
+            public Node()
+            {
+                lc = rc = null;
+                Xmid = -1;
+                leftList = new Vector<Point>();
+                rightList = new Vector<Point>();
+            }
+        }
+        private Node root;
+        public IntervalTree(Vector<T> arr)
+        {
+            //初始化线段端点数组
+            Vector<Point> segmentEndpointArr = new Vector<Point>();
+            for (int i = 0; i < arr.size(); i++)
+            {
+                segmentEndpointArr.append(new Point(arr[i].getStartPoint(), true, arr[i]));
+                segmentEndpointArr.append(new Point(arr[i].getEndPoint(), false, arr[i]));
+            }
+            //线段端点数组排序
+            //需要注意的是，此处排序一次，建树时做的操作不会影响端点的顺序性，所以之后无需重新排序
+            Algorithm.quickSort(segmentEndpointArr, new PointComparer());
+            //建树
+            root = construct(segmentEndpointArr);
+        }
+        private Node construct(Vector<Point> segmentEndpointArr)
+        {
+            //平凡情况下，叶子节点为空
+            if (segmentEndpointArr.size() == 0) return null;
+            //创建节点
+            Node node = new Node();
+            //找中位数Median点位置
+            int medianPointPos = (segmentEndpointArr.size() - 1) / 2;
+            //确定中位数点坐标
+            node.Xmid = segmentEndpointArr[medianPointPos].pos;
+            //分配S{left},S{right}端点的数组
+            Vector<Point> lSegmentEndpointArr = new Vector<Point>();
+            Vector<Point> rSegmentEndpointArr = new Vector<Point>();
+            //遍历全部端点
+            for (int i = 0; i < segmentEndpointArr.size(); i++)
+            {
+                //取交结果
+                int intersectionResult = segmentEndpointArr[i].obj.InterSection(node.Xmid);
+                if (intersectionResult < 0) //线段在点左侧
+                {
+                    //分配到S{left}集合做递归处理
+                    lSegmentEndpointArr.append(segmentEndpointArr[i]);
+                }
+                else if (intersectionResult > 0) //线段在点右侧
+                {
+                    //分配到S{right}集合做递归处理
+                    rSegmentEndpointArr.append(segmentEndpointArr[i]);
+                }
+                else //线段与点相交
+                {
+                    if (segmentEndpointArr[i].isStart) //是线段的左端点
+                    {
+                        //加入左关联列表
+                        node.leftList.append(segmentEndpointArr[i]);
+                    }
+                    else //是线段的右端点
+                    {
+                        //加入右关联列表
+                        node.rightList.append(segmentEndpointArr[i]);
+                    }
+                }
+            }
+            node.lc = construct(lSegmentEndpointArr);
+            node.rc = construct(rSegmentEndpointArr);
+            return node;
+        }
+        public Vector<T> query(int Qx)
+        {
+            Vector<T> results = new Vector<T>();
+            query(root, Qx, results);
+            return results;
+        }
+        private void query(Node node, int Qx, Vector<T> results)
+        {
+            if (node == null) return;
+            if (Qx < node.Xmid) //查询的坐标在Xmid左侧
+            {
+                //遍历左关联列表
+                for(int i = 0; i < node.leftList.size(); i++)
+                {
+                    //不交时跳出
+                    if (node.leftList[i].obj.InterSection(Qx) != 0) break;
+                    //相交时报告
+                    results.append(node.leftList[i].obj);
+                }
+                //继续查询S{left}集合
+                query(node.lc, Qx, results);
+            }
+            else if (node.Xmid < Qx) //查询的坐标在Xmid右侧
+            {
+                //遍历右关联列表
+                for (int i = node.rightList.size()-1; i >=0; i--)
+                {
+                    //不交时跳出
+                    if (node.rightList[i].obj.InterSection(Qx) != 0) break;
+                    //相交时报告
+                    results.append(node.rightList[i].obj);
+                }
+                //继续查询S{left}集合
+                query(node.rc, Qx, results);
+            }
+            else //查询的坐标与Xmid一致
+            {
+                //遍历任意关联列表并全部报告
+                for (int i = 0; i < node.leftList.size(); i++)
+                {
+                    results.append(node.leftList[i].obj);
+                }
+            }
         }
     }
     interface ISegment
@@ -20,6 +156,6 @@ namespace nCov_Patient_Tracer.DSA
         int getStartPoint();
         int getEndPoint();
         bool InterSection(ISegment seg);
-        bool InterSection(int point);
+        int InterSection(int point); //小于0线段在点左侧，等于0相交，大于0线段在点右侧
     }
 }
--- nCov-Patient-Tracer/Tests/Test.cs
+++ nCov-Patient-Tracer/Tests/Test.cs
@@ -1,6 +1,7 @@
 ﻿using nCov_Patient_Tracer.DSA;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -66,6 +67,84 @@ namespace nCov_Patient_Tracer.Tests
             }
             return true;
         }
+        private class Segment : ISegment
+        {
+            int s, e;
+            public Segment(int s,int e)
+            {
+                this.s = s;
+                this.e = e;
+            }
+            public int getEndPoint()
+            {
+                return e;
+            }
+
+            public int getStartPoint()
+            {
+                return s;
+            }
+
+            public bool InterSection(ISegment seg)
+            {
+                return getStartPoint() <= getEndPoint() && getStartPoint() <= seg.getEndPoint() &&
+                    seg.getStartPoint() <= getEndPoint() && seg.getStartPoint() <= seg.getEndPoint();
+            }
+
+            public int InterSection(int point)
+            {
+                if (getStartPoint() <= point && point <= getEndPoint()) return 0;
+                else if (point < getStartPoint()) return 1;
+                else return -1;
+            }
+        }
+        private class SegmentComparer : IComparer<Segment>
+        {
+            public int Compare(Segment x, Segment y)
+            {
+                if (x.getStartPoint() != y.getStartPoint()) return x.getStartPoint() - y.getStartPoint();
+                else if (x.getEndPoint() != y.getEndPoint()) return x.getEndPoint() - y.getEndPoint();
+                else return x.GetHashCode() - y.GetHashCode();
+            }
+        }
+        private static Vector<Segment> InterSection_BruteForce(Vector<Segment> segments, int q)
+        {
+            Vector<Segment> results = new Vector<Segment>();
+            for (int i = 0; i < segments.size(); i++)
+            {
+                if (segments[i].InterSection(q) == 0) results.append(segments[i]);
+            }
+            return results;
+        }
+        private static Vector<Segment> InterSection_IntervalTree(Vector<Segment> segments, int q)
+        {
+            IntervalTree<Segment> tree = new IntervalTree<Segment>(segments);
+            return tree.query(q);
+        }
+        private static bool testInterSection()
+        {
+            const int N = 10000;
+            const int s = -10000, e = 10000;
+            Vector<Segment> segs = new Vector<Segment>();
+            Random ra = new Random();
+            for(int i = 0; i < N; i++)
+            {
+                int t1 = ra.Next(s, e), t2 = ra.Next(s, e);
+                segs.append(new Segment(Math.Min(t1,t2),Math.Max(t1,t2)));
+            }
+            int q = ra.Next(s, e);
+            Vector<Segment> results_BF = InterSection_BruteForce(segs, q);
+            Vector<Segment> results_IT = InterSection_IntervalTree(segs, q);
+            Debug.Assert(results_BF.size() == results_IT.size());
+            Algorithm.quickSort(results_BF, new SegmentComparer());
+            Algorithm.quickSort(results_IT, new SegmentComparer());
+           for(int i = 0; i < results_BF.size(); i++)
+            {
+                if (!object.ReferenceEquals(results_BF[i], results_IT[i])) 
+                    return false;
+            }
+            return true;
+        }
         public static void testMain()
         {
             for (int i = 0; i < 1000; i++)
@@ -74,6 +153,8 @@ namespace nCov_Patient_Tracer.Tests
                     MessageBox.Show("排序错误！");
                 if (!testBinarySearch())
                     MessageBox.Show("二分查找错误！");
+                if(!testInterSection())
+                    MessageBox.Show("线段交集错误！");
             }
         }
     }
